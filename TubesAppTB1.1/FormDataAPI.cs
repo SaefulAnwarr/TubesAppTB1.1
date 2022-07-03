433a20c83acfe37762de5c0562a33c02c1ba943c
devadelt <devaraja@student.telkomuniversity.ac.id>
2022-07-04 01:39:28 +0700

    menambah method button user dan button gerai untuk menampilkan data masing-masing

--- TubesAppTB1.1/FormDataAPI.cs
+++ TubesAppTB1.1/FormDataAPI.cs
@@ -22,12 +22,22 @@ namespace TubesAppTB1._1
 
         private void btnUser_Click(object sender, EventArgs e)
         {
-
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7193/");
+            HttpResponseMessage response = client.GetAsync("api/UserData").Result;
+            var result = response.Content.ReadAsAsync<List<UserList>>();
+            var emp = result.Result;
+            dataGridView1.DataSource = emp;
         }
 
         private void btnGerai_Click(object sender, EventArgs e)
         {
-
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7193/");
+            HttpResponseMessage response = client.GetAsync("api/GeraiData").Result;
+            var result = response.Content.ReadAsAsync<List<GeraiList>>();
+            var emp = result.Result;
+            dataGridView1.DataSource = emp;
         }
     }
 }
