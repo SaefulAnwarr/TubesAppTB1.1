9d50ca3a035675a469f08071fb5984028bffb94c
devadelt <devaraja@student.telkomuniversity.ac.id>
2022-07-04 01:37:29 +0700

    menambah class UserList

--- TubesAppTB1.1/UserList.cs
+++ TubesAppTB1.1/UserList.cs
@@ -8,7 +8,9 @@ namespace TubesAppTB1._1
 {
     internal class UserList
     {
-
-
+        public string NamaUser { get; set; }
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
